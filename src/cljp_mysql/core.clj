f083101ee1d0b3d127de3ee672d6a729571a296a
rod <rod@pu-gh.com>
2012-04-07 09:56:58 +0100

    initial commit

---
+++ src/cljp_mysql/core.clj
@@ -0,0 +1,1 @@
+(ns cljp-mysql.core)
---
+++ test/cljp_mysql/test/core.clj
@@ -0,0 +1,6 @@
+(ns cljp-mysql.test.core
+  (:use [cljp-mysql.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
